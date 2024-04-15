a1a19299bfd27d6e254b5414ed8551069bbef0e9
Afy Wahyu <afyww18@gmail.com>
2024-04-15 23:07:31 +0700

    upp

--- app/components/Aboutsection.jsx
+++ app/components/Aboutsection.jsx
@@ -3,47 +3,47 @@ const Aboutsection = () => {
     return (
         <div className="grid grid-cols-1 p-4 xl:p-10">
             <div className="">
-                <div className="p-4 xl:p-20">
-                    <div className="w-full">
-                        <h1 className="text-2xl xl:text-5xl font-semibold font-sans text-center text-sky-900">Why Choose Us</h1>
-                    </div>
-                    <div className="grid grid-cols-1 xl:grid-cols-3 w-fit opacity-80 bg-white rounded-xl">
-                                <div className="space-y-2 xl:space-y-4 p-7 mx-auto">
-                                    <div className="flex space-x-2">
-                                        <svg className="w-10 xl:w-12 h-6 xl:h-8 my-auto" viewBox="0 0 15 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M7.5 10.45C6.7896 10.45 6.10829 10.1603 5.60596 9.64454C5.10363 9.12882 4.82143 8.42935 4.82143 7.7C4.82143 6.97065 5.10363 6.27118 5.60596 5.75546C6.10829 5.23973 6.7896 4.95 7.5 4.95C8.2104 4.95 8.89171 5.23973 9.39404 5.75546C9.89637 6.27118 10.1786 6.97065 10.1786 7.7C10.1786 8.06114 10.1093 8.41873 9.97468 8.75238C9.84007 9.08603 9.64276 9.38918 9.39404 9.64454C9.14531 9.89991 8.85002 10.1025 8.52505 10.2407C8.20007 10.3789 7.85175 10.45 7.5 10.45ZM7.5 0C5.51088 0 3.60322 0.811248 2.1967 2.25528C0.790176 3.69931 0 5.65783 0 7.7C0 13.475 7.5 22 7.5 22C7.5 22 15 13.475 15 7.7C15 5.65783 14.2098 3.69931 12.8033 2.25528C11.3968 0.811248 9.48912 0 7.5 0Z" fill="black" />
-                                        </svg>
-                                        <h1 className="text-lg xl:text-2xl text-left text-black font-semibold my-auto">Location</h1>
-                                    </div>
-                                    <div>
-                                        <h1 className="text-xs xl:text-sm text-left text-black font-light">Jl. Satrio Wibowo III No.73, Tlogosari Kulon, Kec. Pedurungan, Kota Semarang, Jawa Tengah 50196</h1>
-                                    </div>
+                <div className="p-8 xl:p-20">
+                    <div className="space-y-8 xl:space-y-16">
+                        <div>
+                            <h1 className="text-2xl xl:text-5xl font-semibold font-sans text-center text-sky-900">Why Choose Us</h1>
+                        </div>
+                        <div className="grid grid-cols-1 xl:grid-cols-4 w-full gap-4">
+                            <div className="space-y-2 xl:space-y-4 mx-auto">
+                                <div className="">
+                                    <h1 className="text-lg xl:text-2xl text-center text-black font-semibold my-auto">Harga Terjangkau</h1>
                                 </div>
-                                <div className="space-y-2 xl:space-y-4 p-7 xl:mx-auto">
-                                    <div className="flex space-x-2">
-                                        <svg className="w-10 xl:w-12 h-6 xl:h-10 my-auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M14 14H17M14 10H17M9 9.5V8.5M9 9.5H11.0001M9 9.5C7.20116 9.49996 7.00185 9.93222 7.0001 10.8325C6.99834 11.7328 7.00009 12 9.00009 12C11.0001 12 11.0001 12.2055 11.0001 13.1667C11.0001 13.889 11.0001 14.5 9.00009 14.5M9.00009 14.5L9 15.5M9.00009 14.5H7.0001M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-                                        <h1 className="text-lg xl:text-2xl text-left text-black font-semibold my-auto">Price</h1>
-                                    </div>
-                                    <div className="text-center">
-                                        <h1 className="text-xs xl:text-2xl text-left text-black font-light">Start From 400.000</h1>
-                                    </div>
+                                <div>
+                                    <h1 className="text-xs xl:text-sm text-center text-black font-light">Properti kami menawarkan harga yang terjangkau, memberikan nilai yang luar biasa bagi uang Anda. Kami memahami pentingnya memiliki hunian yang nyaman tanpa harus menguras tabungan Anda.</h1>
                                 </div>
-                                <div className="space-y-2 xl:space-y-4 p-7 xl:mx-auto">
-                                    <div className="flex space-x-2">
-                                        <svg className="w-10 xl:w-12 h-6 xl:h-9 my-auto" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill="var(--ci-primary-color, #000000)" d="M440,424V88H352V13.005L88,58.522V424H16v32h86.9L352,490.358V120h56V456h88V424ZM320,453.642,120,426.056V85.478L320,51Z" class="ci-primary" />
-                                            <rect width="32" height="64" x="256" y="232" fill="var(--ci-primary-color, #000000)" class="ci-primary" />
-                                        </svg>
-                                        <h1 className="text-lg xl:text-2xl text-left text-black font-semibold my-auto">Rooms</h1>
-                                    </div>
-                                    <div className="text-center">
-                                        <h1 className="text-xs xl:text-2xl text-left text-black font-light">10 Kamar</h1>
-                                    </div>
+                            </div>
+                            <div className="space-y-2 xl:space-y-4 mx-auto">
+                                <div className="">
+                                    <h1 className="text-lg xl:text-2xl text-center text-black font-semibold my-auto">Letak Strategis</h1>
+                                </div>
+                                <div className="text-center">
+                                    <h1 className="text-xs xl:text-sm text-center text-black font-light">Properti kami terletak di lokasi yang strategis, dengan akses mudah ke fasilitas penting seperti pusat perbelanjaan, sekolah, transportasi umum, dan pusat kesehatan. Lokasi yang strategis membuat kehidupan sehari-hari Anda lebih nyaman dan efisien.</h1>
+                                </div>
+                            </div>
+                            <div className="space-y-2 xl:space-y-4 mx-auto">
+                                <div className="">
+                                    <h1 className="text-lg xl:text-2xl text-center text-black font-semibold my-auto">Letak Strategis</h1>
+                                </div>
+                                <div className="text-center">
+                                    <h1 className="text-xs xl:text-sm text-center text-black font-light">Properti kami terletak di lokasi yang strategis, dengan akses mudah ke fasilitas penting seperti pusat perbelanjaan, sekolah, transportasi umum, dan pusat kesehatan. Lokasi yang strategis membuat kehidupan sehari-hari Anda lebih nyaman dan efisien.</h1>
+                                </div>
+                            </div>
+                            <div className="space-y-2 xl:space-y-4 mx-auto">
+                                <div className="">
+                                    <h1 className="text-lg xl:text-2xl text-center text-black font-semibold my-auto">Letak Strategis</h1>
+                                </div>
+                                <div className="text-center">
+                                    <h1 className="text-xs xl:text-sm text-center text-black font-light">Properti kami terletak di lokasi yang strategis, dengan akses mudah ke fasilitas penting seperti pusat perbelanjaan, sekolah, transportasi umum, dan pusat kesehatan. Lokasi yang strategis membuat kehidupan sehari-hari Anda lebih nyaman dan efisien.</h1>
                                 </div>
                             </div>
 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
